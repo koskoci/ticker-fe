fe0f1be7352d2b0703078cf1445e350e63113402
Balazs Koskoci <Balazs.Koskoci@logmein.com>
2020-01-06 15:27:27 +0100

    Add dummy string endpoint

---
+++ src/Main.elm
@@ -0,0 +1,103 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode exposing (Decoder, field, int, list, string)
+
+
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+type Model
+    = Failure
+    | Loading
+    | Success String
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Loading, pingBackend )
+
+
+type Msg
+    = MorePlease
+    | GotString (Result Http.Error String)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        MorePlease ->
+            ( Loading, pingBackend )
+
+        GotString result ->
+            case result of
+                Ok myString ->
+                    ( Success myString, Cmd.none )
+
+                Err _ ->
+                    ( Failure, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h2 [] [ text "Random Cats" ]
+        , viewGif model
+        ]
+
+
+viewGif : Model -> Html Msg
+viewGif model =
+    case model of
+        Failure ->
+            div []
+                [ text "I could not load a random cat for some reason. "
+                , button [ onClick MorePlease ] [ text "Try Again!" ]
+                ]
+
+        Loading ->
+            text "Loading..."
+
+        Success myString ->
+            div []
+                [ text myString
+                ]
+
+
+renderList : List Int -> Html msg
+renderList lst =
+    ul []
+        (List.map (\l -> li [] [ text (String.fromInt l) ]) lst)
+
+
+pingBackend : Cmd Msg
+pingBackend =
+    Http.request
+        { method = "GET"
+        , body = Http.emptyBody
+        , timeout = Nothing
+        , tracker = Nothing
+        , headers = []
+        , url = "http://localhost:4000/api"
+        , expect = Http.expectString GotString
+        }
+
+
+myDecoder : Decoder (List Int)
+myDecoder =
+    field "data" (field "draw" (Json.Decode.list int))
