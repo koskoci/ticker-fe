bc0ff698b949b106f9083391609a5e31bc1158b9
Balazs Koskoci <Balazs.Koskoci@logmein.com>
2020-01-08 21:39:25 +0100

    Add an allocation
    Also contains:
    Renames
    Refactors and
    Validations

--- src/Main.elm
+++ src/Main.elm
@@ -7,7 +7,7 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http
-import Json.Decode exposing (Decoder, field, int, list, string)
+import Json.Decode exposing (Decoder, decodeString, field, int, list, string)
 import Time
 
 
@@ -22,9 +22,18 @@ main =
 
 type alias Model =
     { backendState : BackendState
-    , startAmount : Int
-    , date : Maybe Date
+    , initialBalance : Maybe Int
+    , startDate : Maybe Date
     , datePicker : DatePicker
+    , portfolio : List Allocation
+    , currentPercentage : Maybe Float
+    , currentTicker : Maybe String
+    }
+
+
+type alias Allocation =
+    { percentage : Float
+    , ticker : String
     }
 
 
@@ -38,6 +47,9 @@ type Msg
     = GotList (Result Http.Error (List Int))
     | SetStartAmount String
     | SetDatePicker DatePicker.Msg
+    | SetCurrentPercentage String
+    | SetCurrentTicker String
+    | AddAllocation
 
 
 init : () -> ( Model, Cmd Msg )
@@ -48,9 +60,12 @@ init _ =
 
         initialModel =
             { backendState = Loading
-            , startAmount = 0
-            , date = Nothing
+            , initialBalance = Nothing
+            , startDate = Nothing
             , datePicker = datePicker
+            , portfolio = []
+            , currentPercentage = Nothing
+            , currentTicker = Nothing
             }
 
         commands =
@@ -74,40 +89,70 @@ update msg model =
                     ( { model | backendState = Failure }, Cmd.none )
 
         SetStartAmount amountString ->
-            let
-                amount =
-                    String.toInt amountString
-            in
-            case amount of
-                Just amountInt ->
-                    ( { model | startAmount = amountInt }, Cmd.none )
-
-                Nothing ->
-                    ( { model | startAmount = 0 }, Cmd.none )
+            ( { model | initialBalance = String.toInt amountString }, Cmd.none )
 
         SetDatePicker subMsg ->
             let
                 ( newDatePicker, dateEvent ) =
-                    DatePicker.update (settings model.datePicker) subMsg model.datePicker
+                    DatePicker.update (datePickerSettings model.datePicker) subMsg model.datePicker
 
-                date =
+                startDate =
                     case dateEvent of
                         DatePicker.Picked newDate ->
                             Just newDate
 
                         _ ->
-                            model.date
+                            model.startDate
             in
             ( { model
-                | date = date
+                | startDate = startDate
                 , datePicker = newDatePicker
               }
             , Cmd.none
             )
 
+        SetCurrentPercentage percentage ->
+            ( { model | currentPercentage = String.toFloat percentage }, Cmd.none )
 
-settings : DatePicker.DatePicker -> DatePicker.Settings
-settings datePicker =
+        SetCurrentTicker ticker ->
+            case String.isEmpty ticker of
+                True ->
+                    ( { model | currentTicker = Nothing }, Cmd.none )
+
+                False ->
+                    ( { model | currentTicker = Just ticker }, Cmd.none )
+
+        AddAllocation ->
+            let
+                remainingPercentage : Float
+                remainingPercentage =
+                    case model.currentPercentage of
+                        Just currentPercentage ->
+                            100 - totalPercentage model - currentPercentage
+
+                        Nothing ->
+                            100 - totalPercentage model
+            in
+            case currentAllocation model of
+                Just allocation ->
+                    if invalidAllocation model then
+                        ( { model | currentPercentage = Nothing }, Cmd.none )
+
+                    else
+                        ( { model
+                            | portfolio = allocation :: model.portfolio
+                            , currentPercentage = Just remainingPercentage
+                            , currentTicker = Nothing
+                          }
+                        , Cmd.none
+                        )
+
+                Nothing ->
+                    ( model, Cmd.none )
+
+
+datePickerSettings : DatePicker.DatePicker -> DatePicker.Settings
+datePickerSettings datePicker =
     let
         isDisabledAfter : Date -> Date -> Bool
         isDisabledAfter laterDate date =
@@ -130,15 +175,26 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
+    let
+        goodToGo =
+            case formSubmittable model of
+                True ->
+                    "Yes"
+
+                False ->
+                    "No"
+    in
     div [ class "container" ]
         [ div [ class "columns" ]
-            [ div [ class "column" ] []
-            , div [ class "column" ]
+            [ div [ class "column" ] [ text ("total: " ++ String.fromFloat (totalPercentage model) ++ "\tOK: " ++ goodToGo) ]
+            , div [ class "column is-half" ]
                 [ h1 [ class "is-size-1 has-text-centered" ] [ text "ticker!" ]
                 , div
                     [ class "box has-background-primary" ]
                     [ viewStartDate model
                     , viewStartAmount model
+                    , viewAllocationAdder model
+                    , viewAllocationLister model
                     ]
                 ]
             , div [ class "column" ] []
@@ -149,9 +205,9 @@ view model =
 viewStartDate : Model -> Html Msg
 viewStartDate model =
     div [ class "columns" ]
-        [ label [ class "column has-text-weight-semibold" ] [ text "Start Date:" ]
+        [ label [ class "column label" ] [ text "Start Date:" ]
         , div [ class "column" ]
-            [ DatePicker.view model.date (settings model.datePicker) model.datePicker
+            [ DatePicker.view model.startDate (datePickerSettings model.datePicker) model.datePicker
                 |> Html.map SetDatePicker
             ]
         ]
@@ -159,17 +215,27 @@ viewStartDate model =
 
 viewStartAmount : Model -> Html Msg
 viewStartAmount model =
+    let
+        initialBalance =
+            case model.initialBalance of
+                Just amount ->
+                    String.fromInt amount
+
+                Nothing ->
+                    ""
+    in
     div [ class "columns" ]
-        [ label [ class "column has-text-weight-semibold" ] [ text "Initial Balance:" ]
+        [ label [ class "column label" ] [ text "Initial Balance:" ]
         , div [ class "column field has-addons" ]
             [ div [ class "control" ]
                 [ a [ class "button is-static" ] [ text "$" ] ]
-            , div [ class "control" ]
+            , div [ class "control fullwidth" ]
                 [ input
                     [ type_ "text"
-                    , value (String.fromInt model.startAmount)
-                    , onInput SetStartAmount
+                    , value initialBalance
+                    , onBlurWithTargetValue SetStartAmount
                     , class "input"
+                    , placeholder "Initial Balance"
                     ]
                     []
                 ]
@@ -177,27 +243,77 @@ viewStartAmount model =
         ]
 
 
-viewData : Model -> Html Msg
-viewData model =
-    case model.backendState of
-        Failure ->
-            div []
-                [ text "I could not load the data . "
+viewAllocationAdder : Model -> Html Msg
+viewAllocationAdder model =
+    let
+        currentTicker =
+            case model.currentTicker of
+                Just string ->
+                    string
+
+                Nothing ->
+                    ""
+
+        currentPercentage =
+            case model.currentPercentage of
+                Just float ->
+                    String.fromFloat float
+
+                Nothing ->
+                    ""
+    in
+    div [ class "columns" ]
+        [ div [ class "column" ]
+            [ label [ class "label" ] [ text "Portfolio:" ] ]
+        , div [ class "column field has-addons" ]
+            [ div [ class "control" ]
+                [ button
+                    [ class "button"
+                    , disabled (invalidAllocation model)
+                    , onClick AddAllocation
+                    ]
+                    [ text "+"
+                    ]
                 ]
-
-        Loading ->
-            text "Loading..."
-
-        Success myList ->
-            div []
-                [ renderList myList
+            , div [ class "control fullwidth" ]
+                [ input
+                    [ type_ "text"
+                    , value currentPercentage
+                    , onBlurWithTargetValue SetCurrentPercentage
+                    , onEnter AddAllocation
+                    , class "input"
+                    , placeholder "percent"
+                    ]
+                    []
                 ]
+            , div [ class "control" ]
+                [ a [ class "button is-static" ] [ text "%" ] ]
+            , div [ class "control fullwidth" ]
+                [ input
+                    [ type_ "text"
+                    , value currentTicker
+                    , onInput SetCurrentTicker
+                    , onEnter AddAllocation
+                    , class "input"
+                    , placeholder "ticker"
+                    ]
+                    [ text "foo" ]
+                ]
+            ]
+        ]
 
 
-renderList : List Int -> Html msg
-renderList lst =
+viewAllocationLister : Model -> Html Msg
+viewAllocationLister model =
     ul []
-        (List.map (\l -> li [] [ text (String.fromInt l) ]) lst)
+        (List.map
+            (\allocation ->
+                li []
+                    [ text (String.fromFloat allocation.percentage ++ ":\t" ++ allocation.ticker)
+                    ]
+            )
+            model.portfolio
+        )
 
 
 pingBackend : Cmd Msg
@@ -216,3 +332,79 @@ pingBackend =
 myDecoder : Decoder (List Int)
 myDecoder =
     field "data" (field "draw" (Json.Decode.list int))
+
+
+onBlurWithTargetValue : (String -> msg) -> Attribute msg
+onBlurWithTargetValue toMsg =
+    on "blur" (Json.Decode.map toMsg targetValue)
+
+
+currentAllocation : Model -> Maybe Allocation
+currentAllocation model =
+    case ( model.currentPercentage, model.currentTicker ) of
+        ( Just percentage, Just ticker ) ->
+            Just (Allocation percentage ticker)
+
+        _ ->
+            Nothing
+
+
+totalPercentage : Model -> Float
+totalPercentage model =
+    model.portfolio
+        |> List.map .percentage
+        |> List.sum
+
+
+formSubmittable : Model -> Bool
+formSubmittable model =
+    case ( totalPercentage model == 100, model.startDate, model.initialBalance ) of
+        ( True, Just _, Just _ ) ->
+            True
+
+        _ ->
+            False
+
+
+onEnter : msg -> Html.Attribute msg
+onEnter msg =
+    let
+        isEnterKey keyCode =
+            if keyCode == 13 then
+                Json.Decode.succeed msg
+
+            else
+                Json.Decode.fail "silent failure :)"
+    in
+    on "keyup" <|
+        Json.Decode.andThen isEnterKey Html.Events.keyCode
+
+
+allocationIncomplete : Model -> Bool
+allocationIncomplete model =
+    case currentAllocation model of
+        Nothing ->
+            True
+
+        _ ->
+            False
+
+
+percentageOverHundred : Model -> Bool
+percentageOverHundred model =
+    case model.currentPercentage of
+        Just float ->
+            totalPercentage model + float > 100
+
+        Nothing ->
+            False
+
+
+invalidAllocation : Model -> Bool
+invalidAllocation model =
+    case ( allocationIncomplete model, percentageOverHundred model ) of
+        ( False, False ) ->
+            False
+
+        _ ->
+            True
